836ec1f65784fff5ab1a87e6e19ccb8abef1a8d4
Tarek Muhammed <tarek.3d.3dmax@gmail.com>
2019-10-31 17:19:32 +0200

    DEPLY 2

--- LaPitza/settings.py
+++ LaPitza/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '614420c65b633a54a47e7ac511f694510b17ecde531cee97'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['lapitza.herokuapp.com','127.0.0.1']
 
